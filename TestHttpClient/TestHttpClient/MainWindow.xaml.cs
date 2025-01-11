8d7c9c0cca700fffd0081982b5efa8c309881a72
Nguyễn Quang Trung <trungnguye.081203@gmail.com>
2025-01-11 22:32:36 +0700

    Create HttpClient

---
+++ TestHttpClient/TestHttpClient/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+
+namespace TestHttpClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        readonly HttpClient client = new HttpClient();
+
+        private async void btnViewHTML_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = txtURL.Text;
+
+            //Call asynchronous network methods
+            //In a try/catch block to handle exceptions
+            try
+            {
+                string responseBody = await client.GetStringAsync(uri);
+                txtContent.Text = responseBody.Trim();
+            }catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Message : {ex.Message}");
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtContent.Text = string.Empty;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e) => Close();
+    }
+}
