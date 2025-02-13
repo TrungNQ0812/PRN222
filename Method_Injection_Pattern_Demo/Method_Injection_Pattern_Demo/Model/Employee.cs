d7bf0a21567ac8f38e7db6b5d2a327fade33f0d6
Nguyễn Quang Trung <trungnguye.081203@gmail.com>
2025-02-14 02:31:12 +0700

    practice

---
+++ Method_Injection_Pattern_Demo/Method_Injection_Pattern_Demo/Model/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Injection_Pattern_Demo.Model
+{
+    public class Employee
+    {
+        public int EmployeeID;
+        public string EmployeeName;
+        public IDepartment employeeDept;
+        //Default Constructor added for .NET Core DI Container.
+        //So that is can automatically create the instance.
+        public Employee() { }
+        //--------------------------------------
+        public Employee(int id, string name)
+        {
+            EmployeeID = id;
+            EmployeeName = name;
+        }
+
+        //---------------------------------------
+
+        public void AssignDepartment(IDepartment dept)
+        {
+            employeeDept = dept ?? throw new ArgumentNullException("null");
+            //Other business logic if required
+        }
+        //----------------------------------------
+        public override string ToString()
+        {
+            return $"EmpID: {EmployeeID}, Emp name: {EmployeeName}," +
+                $"Department: {employeeDept.DeptName}";
+        }
+    }
+}
---
+++ Method_Injection_Pattern_Demo/Method_Injection_Pattern_Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Method_Injection_Pattern_Demo.Model;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Method_Injection_Pattern_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection()
+                .AddTransient<IDepartment, Marketing>()
+                .AddTransient<Employee>()
+                .BuildServiceProvider();
+
+            Employee emp1 = services.GetService<Employee>();
+            emp1.EmployeeID = 1;
+            emp1.EmployeeName = "Jack";
+            emp1.AssignDepartment(services.GetService<IDepartment>());
+
+            Employee emp2 = services.GetService<Employee>();
+            emp2.AssignDepartment(services.GetService<IDepartment>());
+            emp2.EmployeeID = 2;
+            emp2.EmployeeName = "David";
+
+            Console.WriteLine(emp1);
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine(emp2);
+            Console.ReadLine();
+        }//End main
+    }//End program
+}
